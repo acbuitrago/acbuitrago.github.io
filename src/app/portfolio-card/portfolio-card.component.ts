c97e819b10284a3c70f267df8740da6a4d411ae5
Andrea Buitrago <ac.buitrago403@uniandes.edu.co>
2020-04-15 17:32:17 -0500

    Pills and animations in portfolio card.

--- src/app/portfolio-card/portfolio-card.component.ts
+++ src/app/portfolio-card/portfolio-card.component.ts
@@ -1,9 +1,43 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate, group, query, animateChild, sequence } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio-card',
   templateUrl: './portfolio-card.component.html',
-  styleUrls: ['./portfolio-card.component.scss']
+  styleUrls: ['./portfolio-card.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', [
+        sequence([
+          animate(100),
+          query('@Bounce', animateChild())
+        ])
+      ])
+    ]),
+    trigger('EnterLeave', [
+      state('flyIn', style({ transform: 'translateX(0)' })),
+      transition(':enter', [
+        style({ transform: 'translateY(-150%)', opacity: 0 }),
+        animate('0.3s ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.1s ease-out', style({ transform: 'translateY(-150%)' }))
+      ])
+    ]),
+    trigger('Bounce', [
+      state('flyIn', style({ transform: 'scale(1)' })),
+      transition(':enter', [
+        style({ transform: 'scale(0.1)', opacity: 0 }),
+        animate('0.3s ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.1s ease-out', style({ transform: 'scale(0.1)' }))
+      ])
+    ])
+  ]
 })
 export class PortfolioCardComponent implements OnInit {
 
@@ -15,6 +49,11 @@ export class PortfolioCardComponent implements OnInit {
 
   @Input()
   summary: string;
+
+  @Input()
+  techs: string[];
+
+  show = false;
 
   constructor() { }
 
